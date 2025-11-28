0ea7270c62b20955af2256e663ce0b7215894ac0
xian188 <127485554+xian188@users.noreply.github.com>
2025-11-29 00:14:23 +0800

    Update test_string_iterable.py

--- tests/readers/test_string_iterable.py
+++ tests/readers/test_string_iterable.py
@@ -8,3 +8,13 @@ def test_load() -> None:
     reader = StringIterableReader()
     documents = reader.load_data(texts=["I went to the store", "I bought an apple"])
     assert len(documents) == 2
+def test_failed_request(mocked_responses: responses.RequestsMock) -> None:
+    """Test that a failed request raises an error."""
+    path = "chains/path/chain.json"
+    loader = Mock()
+
+    mocked_responses.get(urljoin(URL_BASE.format(ref=DEFAULT_REF), path), status=500)
+
+    with pytest.raises(ValueError, match=re.compile("Could not find file at .*")):
+        try_load_from_hub(f"lc://{path}", loader, "chains", {"json"})
+    loader.assert_not_called()
